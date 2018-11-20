5e4b4701e48aacb7192f293b3a92cb0d7d319253
István Donkó <istvan.donko+github@gmail.com>
2018-11-20 02:13:50 +0100

    Preparing statement parameter substitution

--- src/neo4j_driver.erl
+++ src/neo4j_driver.erl
@@ -18,14 +18,13 @@ init() ->
 
 options(Host, Port, Username, Password) ->
   [
-    {host, Host},
-    {port, Port},
-    {username, Username},
-    {password, Password}
+    { host, Host },
+    { port, Port },
+    { username, Username },
+    { password, Password }
   ].
 
-run(Options, Statement) ->
-  St = make_statement(Statement),
+run(Options, Statements) ->
   Url = (
     "http://" ++
     proplists:get_value(username, Options) ++
@@ -37,27 +36,80 @@ run(Options, Statement) ->
     proplists:get_value(port, Options) ++
     "/db/data/transaction/commit"
   ),
-  send_post(Url, St).
+  send_post(Url, make_statements(Statements)).
 
 test() ->
   neo4j_driver:init(),
   Options = neo4j_driver:options("localhost", "7474", "test", "valami"),
-  neo4j_driver:run(Options,"CREATE (n)").
+  Statements = [
+    [
+      { statement, "CREATE (a:Person {name: $name}) RETURN a" },
+      { parameters, [ { "name", "Valaki" } ] }
+    ],
+    [
+      { statement, "CREATE (n:Sajt)" }
+    ],
+    [
+      { statement, "CREATE (n {props}) RETURN n" },
+      { parameters, [
+          { "props", [
+              { "name", "My node" }
+            ]
+          }
+        ]
+      }
+    ]
+  ],
+  neo4j_driver:run(Options, Statements).
+
+% "parameters" : {
+%       "props" : {
+%         "name" : "My Node"
+%       }
+%     }
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
 
-make_statement(S) ->
+% make_parameter_binary(Parameter) ->
+%   { Key, Value } = Parameter,
+%   { list_to_binary(Key), list_to_binary(Value) }.
+
+make_parameter_binary({ Key, Value }) ->
+  { make_parameter_binary(Key), make_parameter_binary(Value) };
+make_parameter_binary(List) ->
+  lists:map(fun make_parameter_binary/1, List);
+make_parameter_binary(String) ->
+  list_to_binary(String).
+
+% make_statement(Statement) ->
+%   [
+%     { <<"statement">>, list_to_binary(Statement) }
+%   ].
+make_statement(Statement) ->
   [
     {
-      <<"statements">>, [
-        [
-          {<<"statement">>, list_to_binary(S)}
-        ]
-      ]
+      <<"statement">>,
+      list_to_binary(proplists:get_value(statement, Statement))
+    },
+    {
+      <<"parameters">>,
+      lists:map(
+        fun make_parameter_binary/1,
+        proplists:get_value(parameters, Statement, [])
+      )
     }
   ].
+
+make_statements(Statements) ->
+  A = [
+    {
+      <<"statements">>, lists:map(fun make_statement/1, Statements)
+    }
+  ],
+  io:format("Sajt: ~p", A),
+  A.
 
 send_post(Url, Body) ->
   restc:request(post, json, Url, [], [], Body, []).
